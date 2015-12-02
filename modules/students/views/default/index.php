6d3350719f5f6c59d0125c791502fa260888ab91
k1llforl1ve <k1llforl1ve@yahoo.com>
2015-12-02 12:46:10 +0200

    изменил тут то

--- modules/students/views/default/index.php
+++ modules/students/views/default/index.php
@@ -12,7 +12,7 @@ $this->title = 'Students';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="students-index">
-ы
+видал€ю
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
     <p>
